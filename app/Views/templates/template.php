1c19b2c1b95dcbd7f5bc1ac77eefc020528ee5f4
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-10-09 09:09:45 +0700

    tambahin viewnya yang udh di buat rifan

--- app/Views/templates/template.php
+++ app/Views/templates/template.php
@@ -8,27 +8,24 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="/assets/customcss/style.css">
 
 </head>
 
 <body>
-    <main>
-        <div class="container-fluid">
-            <?= $this->renderSection('content') ?>
-        </div>
-    </main>
+    <!-- Menggunakan file navbar -->
+    <div class="container-fluid">
+        <?= $this->include('/partials/navbar'); ?>
+    </div>
+    <!-- Section -->
+    <div class="container mt-4">
+        <?= $this->renderSection('content'); ?>
+    </div>
+
 </body>
+
+
 <script src="/jquery/jquery.min.js"></script>
 <script src="/bootstrap/js/bootstrap.bundle.js"></script>
-<script>
-    $('#ulangi-password').change(function(e) {
-        e.preventDefault();
-        if ($('#password').val() != $('#ulangi-password').val()) {
-            $('#ulangi-password').addClass('is-invalid');
-        } else {
-            $('#ulangi-password').removeClass('is-invalid');
-        }
-    });
-</script>
 
 </html>
